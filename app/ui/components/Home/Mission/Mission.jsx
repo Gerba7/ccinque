367669c0ce6ccc7b215c465acdcb4482443533cc
Gerba <gmlongobardi@hotmail.com>
2024-08-09 18:14:45 -0300

    V1.4

--- app/ui/components/Home/Mission/Mission.jsx
+++ app/ui/components/Home/Mission/Mission.jsx
@@ -22,7 +22,7 @@ const Mission = () => {
         setAnimate(false);
         }
     },
-    { threshold: 0 }
+    { threshold: 0.5 }
     );
 
     if (animationRef.current) {
@@ -43,10 +43,10 @@ const Mission = () => {
   return (
     <div className={styles.container}>
         <div className={styles.scrollOffset} id='objetivos'></div>
-        <div className={styles.wrapper} ref={animationRef}>
+        <div className={styles.wrapper}>
             <h3 className={`${styles.subtitle} ${animate ? styles.visible : ''}`}>NUESTROS</h3>
             <h1 className={`${styles.title} ${animate ? styles.visible : ''}`}>Objetivos</h1>
-            <p className={`${styles.description} ${animate ? styles.visible : ''}`}>
+            <p className={`${styles.description} ${animate ? styles.visible : ''}`} ref={animationRef}>
               Nuestro principal objetivo es lograr una buena salud bucal en nuestros pacientes, 
               haciendo énfasis en la educación para la salud y en la prevención, 
               realizando constantes monitoreos en adultos y en niños. 
